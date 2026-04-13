078444bb374b6820791d28c3dd46a2102b1cf9d3
Juan Gilsanz Polo <juangilsanz@gmail.com>
2026-04-13 15:00:45 +0200

    1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jgeek00.crowdsecmonitor"
         minSdk = 31
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
